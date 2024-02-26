0ba06b3d6fd04c48f36c786b6503ff2d1b14b045
25Ommy <156078030+25Ommy@users.noreply.github.com>
2024-02-26 11:52:17 -0800

    Chore:created a python file that prompts user to enter inputs

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input ('What is your name?')
+age = int(input('How old are you?'))
+location = input('What is your your location?')
+
+print(f'Hello {name} you are {age} and your location is {location}')
